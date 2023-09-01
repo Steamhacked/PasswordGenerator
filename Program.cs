34a8d8c58db5e14c73e1be835c3da90e934458c5
alen.syzdykov <67708657+Steamhacked@users.noreply.github.com>
2023-09-01 14:03:05 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace cvbooster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] pw = new char[10];
+            Random rnd = new Random();
+
+            string str1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string str2 = "abcdefghijklmnopqrstuvwxyz";
+            string str3 = "!@#$%^&*()_+-=:;'\"|?/\\`~";
+                
+            for (int i=0; i<7; i+=2)
+            {
+                int n = rnd.Next(0,25);
+                pw[i] = str1[n];
+            }
+
+            for(int i=1; i<8; i += 2)
+            {
+                int n = rnd.Next(0,25);
+                pw[i] = str2[n];
+            }
+
+            for(int i=8; i<10; i++)
+            {
+                int n = rnd.Next(0,23);
+                pw[i] = str3[n];
+            }
+
+            Console.WriteLine(pw);
+        }
+    }
+}
